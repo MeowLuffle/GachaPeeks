79fb9e6d5f91d58bee37095dd8441a6bda444918
ObsidianPhantom <nightly.obsidian.raven@gmail.com>
2024-10-31 20:31:10 +0100

    initialize project structure and configuration files - that's a lot of changes (⟃ ͜ʖ ⟄)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let count = 0;
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="flex-col justify-center space-y-5">
+		<h1 class="h1">Let's get cracking bones!</h1>
+		<p>Start by exploring:</p>
+		<ul>
+			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
+			<li><code class="code">/src/app.postcss</code> - app wide css</li>
+			<li>
+				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+			</li>
+		</ul>
+		<div class="flex justify-center space-x-5">
+			<button class="btn variant-ghost" on:click={() => (count += 1)}>
+				Clicked {count}
+				{count === 1 ? 'time' : 'times'}
+			</button>
+			<button class="btn variant-ghost" on:click={() => (count = 0)}> Reset </button>
+		</div>
+	</div>
+</div>
